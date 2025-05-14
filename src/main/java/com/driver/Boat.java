bc5884414f1972f4960ea1961a54759231972286
Mohammed_Rizwan <mdrizwanalihussain@gmail.com>
2025-05-14 17:01:04 +0530

    Errors Solved

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,20 +2,20 @@ package com.driver;
 
 public class Boat implements WaterVehicle{
 
-    String name = "Boat";
-    int capacity = 20;
+    private String name;
+    private int capacity;
 
-    public String getVehicleName() {
+    public Boat(String name, int capacity) {
+		super();
+		this.name = name;
+		this.capacity = capacity;
+	}
+
+	public String getVehicleName() {
         return name;
     }
 
     public int getVehicleCapacity() {
         return capacity;
-    }
-
-    public static void main (String[] args) {
-        Boat boat = new Boat();
-        System.out.println(boat.getVehicleName());
-        System.out.println(boat.getVehicleCapacity());
     }
 }
